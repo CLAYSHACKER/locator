b7761d31c29a1d58231eb5505393281688e31c82
CLAYS HACKER <37822373+CLAYSHACKER@users.noreply.github.com>
2019-10-18 17:51:07 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pkg install wget -y
+pkg install openssh -y
+pkg install php -y
+pkg install figlet -y
+
+clear
+
+setterm -foreground green
+figlet "clays hacker ningun sistema es seguro" 
